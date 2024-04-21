9ef25f2e077be9b060ce0dfd9344352810bd3293
insub2004 <insub2004@naver.com>
2024-04-22 00:35:07 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/hellospringprofile2/HelloSpringProfile2Application.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.hellospringprofile2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringProfile2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringProfile2Application.class, args);
+    }
+
+}
